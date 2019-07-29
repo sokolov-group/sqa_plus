ab48232bbc4613a527d8c2db750d29d6c35a6992
Ilia Mazin <mazin.3@osu.edu>
2019-07-29 12:45:41 -0400

    Added all relevant h0-h1 S matrix equations

---
+++ ilia_examples/M_matrix/h0_h1/semi-internal/H_zero/EE_M0_ca_aaae.py
@@ -0,0 +1,36 @@
+import sqa_extra.secondQuantizationAlgebra as sqa
+
+sqa.options.verbose = True
+
+# Define operator types
+tg_c = sqa.options.core_type
+tg_a = sqa.options.active_type
+tg_v = sqa.options.virtual_type
+tg_g = tg_c + tg_a + tg_v
+
+# Define indices
+dummy = True
+
+# External indices
+i = sqa.index('I', [tg_c])
+x = sqa.index('X', [tg_a])
+
+y = sqa.index('Y', [tg_a])
+z = sqa.index('Z', [tg_a])
+b = sqa.index('B', [tg_v])
+w = sqa.index('W', [tg_a])
+
+l_op = [sqa.creOp(i), sqa.desOp(x)]
+r_op = [sqa.creOp(b), sqa.creOp(w), sqa.desOp(z), sqa.desOp(y)]
+
+effH = []
+effH = sqa.Heff(0)
+
+term1 = sqa.term(1.0, [], r_op)
+term2 = sqa.term(1.0, [], l_op)
+
+term3 = sqa.commutator(effH, term1)
+term4 = sqa.commutator(term2, term3)
+term5 = sqa.matrixBlock(term4)
+
+sqa.generateEinsum(term5, 'temp', 'IXYZBW', "")
