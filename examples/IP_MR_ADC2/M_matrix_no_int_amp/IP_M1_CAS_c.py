218763e70dea82234fab06c0239715b46ffe6c0c
Alexander Sokolov <sokolov.8@osu.edu>
2020-02-04 13:14:43 -0500

    Cleaned up all examples, removed large parts of old output files

---
+++ examples/IP_MR_ADC2/M_matrix_no_int_amp/IP_M1_CAS_c.py
@@ -0,0 +1,40 @@
+import sqa_extra.secondQuantizationAlgebra as sqa
+
+sqa.options.verbose = True
+
+# Define operator types
+tg_c = sqa.options.core_type
+tg_a = sqa.options.active_type
+tg_v = sqa.options.virtual_type
+tg_g = tg_c + tg_a + tg_v
+
+# Define indices
+dummy = True
+
+# External indices
+i = sqa.index('I', [tg_c])
+j = sqa.index('J', [tg_c])
+
+i_op = [sqa.desOp(i)]
+j_op = [sqa.creOp(j)]
+
+effH = []
+effH = sqa.Heff(1)
+for t in effH:
+  print t
+
+term1 = sqa.term(1.0, [], i_op)
+term2 = sqa.term(1.0, [], j_op)
+
+print "First Commutator"
+term3 = sqa.commutator(effH,term2)
+for t in term3:
+    print t
+
+term5 = sqa.matrixBlock(term3)
+
+sqa.generateEinsum(term5, 'M[s_casci_a:f_casci_a, s_c:f_c]', 'J', transRDM=True, trans_ind_str="I")
+#
+#
+#
+##################################################################
