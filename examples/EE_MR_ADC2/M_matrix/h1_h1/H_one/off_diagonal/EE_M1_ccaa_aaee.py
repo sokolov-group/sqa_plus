b29349e2b257774a02c8a33b681f96619d59dfa3
Ilia Mazin <mazin.3@osu.edu>
2020-02-24 15:52:22 -0500

    Generated upper-triangle -X equations for EE-MR-ADC

---
+++ examples/EE_MR_ADC2/M_matrix/h1_h1/H_one/off_diagonal/EE_M1_ccaa_aaee.py
@@ -0,0 +1,42 @@
+import sqa_extra.secondQuantizationAlgebra as sqa
+
+# Define operator types
+tg_c = sqa.options.core_type
+tg_a = sqa.options.active_type
+tg_v = sqa.options.virtual_type
+tg_g = tg_c + tg_a + tg_v
+
+# Define indices
+dummy = True
+
+# External indices
+i = sqa.index('I', [tg_c])
+j = sqa.index('J', [tg_c])
+x = sqa.index('X', [tg_a])
+y = sqa.index('Y', [tg_a])
+
+z = sqa.index('Z', [tg_a])
+u = sqa.index('U', [tg_a])
+a = sqa.index('A', [tg_v])
+b = sqa.index('B', [tg_v])
+
+l_op = [sqa.creOp(i), sqa.creOp(j), sqa.desOp(y), sqa.desOp(x)]
+r_op = [sqa.creOp(a), sqa.creOp(b), sqa.desOp(u), sqa.desOp(z)]
+
+effH = []
+effH = sqa.Heff(1)
+
+for t in effH:
+  print (t)
+
+term1 = sqa.term(1.0, [], r_op)
+term2 = sqa.term(1.0, [], l_op)
+
+print ("First Commutator")
+
+term3 = sqa.commutator(effH, term1)
+term4 = sqa.commutator(term2, term3)
+
+term5 = sqa.matrixBlock(term4)
+
+sqa.generateEinsum(term5, 'temp', 'IJXYZUAB', "")
---
+++ examples/EE_MR_ADC2/M_matrix/h1_h1/H_one/off_diagonal/EE_M1_ccaa_caaa.py
@@ -0,0 +1,39 @@
+import sqa_extra.secondQuantizationAlgebra as sqa
+
+# Define operator types
+tg_c = sqa.options.core_type
+tg_a = sqa.options.active_type
+tg_v = sqa.options.virtual_type
+tg_g = tg_c + tg_a + tg_v
+
+# External indices
+i = sqa.index('I', [tg_c])
+j = sqa.index('J', [tg_c])
+x = sqa.index('X', [tg_a])
+y = sqa.index('Y', [tg_a])
+
+k = sqa.index('K', [tg_c])
+z = sqa.index('Z', [tg_a])
+u = sqa.index('U', [tg_a])
+v = sqa.index('V', [tg_a])
+
+l_op = [sqa.creOp(i), sqa.creOp(j), sqa.desOp(y), sqa.desOp(x)]
+r_op = [sqa.creOp(u), sqa.creOp(v), sqa.desOp(z), sqa.desOp(k)]
+
+effH = []
+effH = sqa.Heff(1)
+
+for t in effH:
+  print (t)
+
+term1 = sqa.term(1.0, [], r_op)
+term2 = sqa.term(1.0, [], l_op)
+
+print ("First Commutator")
+
+term3 = sqa.commutator(effH, term1)
+term4 = sqa.commutator(term2, term3)
+
+term5 = sqa.matrixBlock(term4)
+
+sqa.generateEinsum(term5, 'temp', 'IJXYKZUV', "")
