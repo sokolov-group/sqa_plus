b7717e927f3aeed38d0270ee50b9b65efd99cd85
Donna Odhiambo <odhiambo.5@osu.edu>
2025-11-04 18:45:53 -0500

    removed commented-out code in sqaSymmetry

--- sqaSymmetry.py
+++ sqaSymmetry.py
@@ -40,31 +40,6 @@ class symmetry:
     #------------------------------------------------------------------------------------------------
 
     def __init__(self,pattern,factor):
-        ##patternError = False
-        ##if type(pattern) != type((1,2)):
-        ##    patternError = True
-        ##else:
-        ##    tempList = []
-        ##    for elem in pattern:
-        ##        if type(elem) != type(1):
-        ##            patternError = True
-        ##            break
-        ##        else:
-        ##            tempList.append(elem)
-        ##    if not patternError:
-        ##        tempList.sort()
-        ##        patternError = ( tempList != range(len(pattern)) )
-        ##if patternError:
-        ##    raise ValueError("pattern must be a tuple of contiguous, non-negative integers including zero. They need not be in order.")
-        ##else:
-        ##    self.pattern = pattern
-        ##if (factor == 1 or factor == -1) and type(factor) == type(1):
-        ##    self.factor = factor
-        ##elif type(factor) == type(1) or type(factor) == type(1.0):
-        ##    self.factor = float(factor)
-        ##else:
-        ##    raise TypeError("factor must be a float or an int.")
-
         patternError = False
 
         if not isinstance(pattern, tuple) or not all(isinstance(elem, int) for elem in pattern):
@@ -88,12 +63,6 @@ class symmetry:
 
     #------------------------------------------------------------------------------------------------
     
-    #def __cmp__(self,other):
-    #    if not isinstance(other,symmetry):
-    #        raise TypeError("can only compare a symmetry object to other symmetry objects")
-    #    return cmp(self.pattern,other.pattern)
-
-
     def __eq__(self, other):
         if not isinstance(other, symmetry):
             raise TypeError("can only compare a symmetry object to other symmetry objects")
