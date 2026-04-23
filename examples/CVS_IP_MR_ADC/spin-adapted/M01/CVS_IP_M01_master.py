4c67d1bdb8c9e3a8512cb2eaa215f11f9aded7cc
Donna Odhiambo <donna.odhiambo@proton.me>
2026-04-23 16:27:56 -0400

    new cvs-ip m01 eqns

--- examples/CVS_IP_MR_ADC/spin-adapted/M01/CVS_IP_M01_master.py
+++ examples/CVS_IP_MR_ADC/spin-adapted/M01/CVS_IP_M01_master.py
@@ -1,171 +1,104 @@
+import time
 import sqa_plus
-sqa_plus.options.spin_integrated = True
-sqa_plus.options.cvs_approach = True
+from sqa_plus import options
+
+options.cvs_approach = True
+options.spin_integrated = True
+
+#order_Heff = 0
 order_Heff = 1
 
-import time
+indices_string = 'c_cva'
+spin_indices_string = 'a_aaa'
+
 start = time.time()
-
-indices_string = 'c_caa'
-spin_indices_string = 'a_bab'
-
-sqa_plus.options.print_header("Spin-Adapted CVS-IP: M01")
+options.print_header("Spin-Adapted CVS-IP: M01 H{:}".format(order_Heff))
 
 # Generating operators
 print("\n## Generating operators ...\n")
 
 ## Define indices
-tg_cvs_cor = sqa_plus.options.cvs_core_type
-tg_cvs_val = sqa_plus.options.cvs_valence_type
-tg_act = sqa_plus.options.active_type
-tg_vir = sqa_plus.options.virtual_type
+tg_cvs_cor = options.cvs_core_type
+tg_cvs_val = options.cvs_valence_type
+tg_act = options.active_type
+tg_vir = options.virtual_type
 
-tg_alpha = sqa_plus.options.alpha_type
-tg_beta = sqa_plus.options.beta_type
+tg_a = options.alpha_type
+tg_b = options.beta_type
 
 ## External Indices
-a_alpha = sqa_plus.index('A', [tg_alpha, tg_vir])
-a_beta  = sqa_plus.index('A', [tg_beta,  tg_vir])
+b_alpha = sqa_plus.index('B', [tg_a, tg_vir])
+b_beta  = sqa_plus.index('B', [tg_b, tg_vir])
 
-b_alpha = sqa_plus.index('B', [tg_alpha, tg_vir])
-b_beta  = sqa_plus.index('B', [tg_beta,  tg_vir])
+i_alpha = sqa_plus.index('I', [tg_a, tg_cvs_cor])
+i_beta  = sqa_plus.index('I', [tg_b, tg_cvs_cor])
 
-i_alpha = sqa_plus.index('I', [tg_alpha, tg_cvs_cor])
-i_beta  = sqa_plus.index('I', [tg_beta,  tg_cvs_cor])
+k_alpha = sqa_plus.index('K', [tg_a, tg_cvs_cor])
+k_beta  = sqa_plus.index('K', [tg_b, tg_cvs_cor])
 
-j_alpha = sqa_plus.index('J', [tg_alpha, tg_cvs_cor])
-j_beta  = sqa_plus.index('J', [tg_beta,  tg_cvs_cor])
+l_alpha = sqa_plus.index('L', [tg_a, tg_cvs_cor])
+l_beta  = sqa_plus.index('L', [tg_b, tg_cvs_cor])
 
-k_alpha = sqa_plus.index('K', [tg_alpha, tg_cvs_cor])
-k_beta  = sqa_plus.index('K', [tg_beta,  tg_cvs_cor])
+l_val_alpha = sqa_plus.index('L', [tg_a, tg_cvs_val])
+l_val_beta  = sqa_plus.index('L', [tg_b, tg_cvs_val])
 
-l_alpha = sqa_plus.index('L', [tg_alpha, tg_cvs_cor])
-l_beta  = sqa_plus.index('L', [tg_beta,  tg_cvs_cor])
+w_alpha = sqa_plus.index('W', [tg_a, tg_act])
+w_beta  = sqa_plus.index('W', [tg_b, tg_act])
 
-l_val_alpha = sqa_plus.index('L', [tg_alpha, tg_cvs_val])
-l_val_beta  = sqa_plus.index('L', [tg_beta,  tg_cvs_val])
+y_alpha = sqa_plus.index('Y', [tg_a, tg_act])
+y_beta  = sqa_plus.index('Y', [tg_b, tg_act])
 
-x_alpha = sqa_plus.index('X', [tg_alpha, tg_act])
-x_beta  = sqa_plus.index('X', [tg_beta,  tg_act])
+z_alpha = sqa_plus.index('Z', [tg_a, tg_act])
+z_beta  = sqa_plus.index('Z', [tg_b, tg_act])
 
-y_alpha = sqa_plus.index('Y', [tg_alpha, tg_act])
-y_beta  = sqa_plus.index('Y', [tg_beta,  tg_act])
-
-w_alpha = sqa_plus.index('W', [tg_alpha, tg_act])
-w_beta  = sqa_plus.index('W', [tg_beta,  tg_act])
-
-z_alpha = sqa_plus.index('Z', [tg_alpha, tg_act])
-z_beta  = sqa_plus.index('Z', [tg_beta,  tg_act])
+i     = {'a': i_alpha,     'b': i_beta}
+k     = {'a': k_alpha,     'b': k_beta}
+l     = {'a': l_alpha,     'b': l_beta}
+l_val = {'a': l_val_alpha, 'b': l_val_beta}
+w     = {'a': w_alpha,     'b': w_beta}
+y     = {'a': y_alpha,     'b': y_beta}
+z     = {'a': z_alpha,     'b': z_beta}
+b     = {'a': b_alpha,     'b': b_beta}
 
 ## Define terms
-if indices_string in ['c_caa']:
-    if spin_indices_string == 'a_aaa':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(w_alpha), sqa_plus.desOp(z_alpha)])
+ops = {
+    'c_caa': (i, k, w,     z, 'IKWZ'),
+    'c_cce': (i, k, l,     b, 'IKLB'),
+    'c_cve': (i, k, l_val, b, 'IKLB'),
+    'c_cca': (i, k, l,     y, 'IKLY'),
+    'c_cva': (i, k, l_val, y, 'IKLY'),
+    'c_cae': (i, k, y,     b, 'IKYB'),
+}
 
-    elif spin_indices_string == 'a_abb':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(w_beta), sqa_plus.desOp(z_beta)])
+spins = {
+    'a_aaa': ('a', 'a', 'a', 'a'),
+    'a_abb': ('a', 'a', 'b', 'b'),
+    'a_bab': ('a', 'b', 'a', 'b'),
+}
 
-    elif spin_indices_string == 'a_bab':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_beta), sqa_plus.creOp(w_alpha), sqa_plus.desOp(z_beta)])
+l_ind, *r_ind = ops[indices_string]
+s1, s2, s3, s4 = spins[spin_indices_string]
 
-    final_indices_string = 'IKWZ'
-    indices_string = indices_string + '_' + spin_indices_string
+term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(l_ind[s1])])
+term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(r_ind[0][s2]), sqa_plus.creOp(r_ind[1][s3]), sqa_plus.desOp(r_ind[2][s4])])
+final_indices_string = r_ind[3]
+indices_string = indices_string + '_' + spin_indices_string
 
-elif indices_string in ['c_cce']:
-    if spin_indices_string == 'a_aaa':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_alpha), sqa_plus.desOp(b_alpha)])
-
-    elif spin_indices_string == 'a_abb':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_beta), sqa_plus.desOp(b_beta)])
-
-    elif spin_indices_string == 'a_bab':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_beta), sqa_plus.creOp(l_alpha), sqa_plus.desOp(b_beta)])
-
-    final_indices_string = 'IKLB'
-    indices_string = indices_string + '_' + spin_indices_string
-
-elif indices_string in ['c_cve']:
-    if spin_indices_string == 'a_aaa':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_val_alpha), sqa_plus.desOp(b_alpha)])
-
-    elif spin_indices_string == 'a_abb':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_val_beta), sqa_plus.desOp(b_beta)])
-
-    elif spin_indices_string == 'a_bab':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_beta), sqa_plus.creOp(l_val_alpha), sqa_plus.desOp(b_beta)])
-
-    final_indices_string = 'IKLB'
-    indices_string = indices_string + '_' + spin_indices_string
-
-elif indices_string in ['c_cca']:
-    if spin_indices_string == 'a_aaa':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_alpha), sqa_plus.desOp(y_alpha)])
-
-    elif spin_indices_string == 'a_abb':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_beta), sqa_plus.desOp(y_beta)])
-
-    elif spin_indices_string == 'a_bab':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_beta), sqa_plus.creOp(l_alpha), sqa_plus.desOp(y_beta)])
-
-    final_indices_string = 'IKLY'
-    indices_string = indices_string + '_' + spin_indices_string
-
-elif indices_string in ['c_cva']:
-    if spin_indices_string == 'a_aaa':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_val_alpha), sqa_plus.desOp(y_alpha)])
-
-    elif spin_indices_string == 'a_abb':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(l_val_beta), sqa_plus.desOp(y_beta)])
-
-    elif spin_indices_string == 'a_bab':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_beta), sqa_plus.creOp(l_val_alpha), sqa_plus.desOp(y_beta)])
-
-    final_indices_string = 'IKLY'
-    indices_string = indices_string + '_' + spin_indices_string
-
-elif indices_string in ['c_cae']:
-    if spin_indices_string == 'a_aaa':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(y_alpha), sqa_plus.desOp(b_alpha)])
-
-    elif spin_indices_string == 'a_abb':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_alpha), sqa_plus.creOp(y_beta), sqa_plus.desOp(b_beta)])
-
-    elif spin_indices_string == 'a_bab':
-        term_left  = sqa_plus.term(1.0, [], [sqa_plus.desOp(i_alpha)])
-        term_right = sqa_plus.term(1.0, [], [sqa_plus.creOp(k_beta), sqa_plus.creOp(y_alpha), sqa_plus.desOp(b_beta)])
-
-    final_indices_string = 'IKYB'
-    indices_string = indices_string + '_' + spin_indices_string
+print("##  Left Op: %s" % term_left)
+print("## Right Op: %s" % term_right)
 
 # Spin-Adapted H_eff
 terms_Heff = sqa_plus.Heff(order_Heff)
 
 ## Calculating the commutator
-print("## Calculating the commutator [H(0), a_S^\dag a_T^\dag a_U] ...")
+print("## Calculating the commutator [H(0), a_S^\\dag a_T^\\dag a_U] ...")
 terms_commutator = sqa_plus.commutator(terms_Heff, term_right)
 
-print("\n## Calculating a_Q [H(0), a_S^\dag a_T^\dag a_U] ...")
+print("\n## Calculating a_Q [H(0), a_S^\\dag a_T^\\dag a_U] ...")
 terms_IP_M01 = []
-for term_commutator in terms_commutator:
-    terms_IP_M01.append(sqa_plus.multiplyTerms(term_commutator, term_left))
-    terms_IP_M01.append(sqa_plus.multiplyTerms(term_left, term_commutator))
+for term_comm in terms_commutator:
+    terms_IP_M01.append(sqa_plus.multiplyTerms(term_comm, term_left))
+    terms_IP_M01.append(sqa_plus.multiplyTerms(term_left, term_comm))
 
 # Expected value of Spin-Adapted IP M01
 expected_IP_M01 = sqa_plus.matrixBlock(terms_IP_M01)
@@ -174,7 +107,9 @@ expected_IP_M01 = sqa_plus.matrixBlock(terms_IP_M01)
 expected_IP_M01_sa = sqa_plus.convertSpinIntegratedToAdapted(expected_IP_M01)
 
 # Generating Numpy einsum equations
-result = sqa_plus.genEinsum(expected_IP_M01_sa, 'M_' + indices_string, final_indices_string)
+options.genEinsum.lhs_string = 'M_' + indices_string
+options.genEinsum.indices_string = final_indices_string
+result = sqa_plus.genEinsum(expected_IP_M01_sa)
 
-end = time.time()
-print("> Total elapsed time: {:.2f} seconds.".format(end - start))
+print("> Total elapsed time: {:.2f} seconds.".format(time.time() - start))
+
